ddeb0d47aee577e0a3b33700913b412052fb99aa
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-19 23:05:55 +0200

    add sliding and fade animation to the text in splash view

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({
+    super.key,
+    required this.slidingAnimation,
+    required this.fadeAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+  final Animation<double> fadeAnimation;
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: fadeAnimation,
+      child: AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Freely',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12, letterSpacing: 5),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,52 @@
 import 'package:bookly/core/utils/assets_data.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _initAnimation();
+  }
+
+  void _initAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero).animate(
+          CurvedAnimation(
+            parent: animationController,
+            curve: Curves.easeOutCubic, // Smoother animation
+          ),
+        );
+
+    fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeIn),
+    );
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose(); // ✅ Important: Dispose controller
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +55,10 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-<<<<<<< HEAD
         const SizedBox(height: 3),
-=======
->>>>>>> 3ed228237fcc5fdcf1ab3de214b605f7079a44f7
-        const Text(
-          'Read Freely',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 12, letterSpacing: 5),
+        SlidingText(
+          slidingAnimation: slidingAnimation,
+          fadeAnimation: fadeAnimation,
         ),
       ],
     );
