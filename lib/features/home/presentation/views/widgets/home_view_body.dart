f37690b594114f765cc2e45d4a56312f6be7e6ec
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-20 20:37:51 +0200

    chore: add .gitattributes to prioritize Dart in stats

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -8,7 +8,29 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(children: [SizedBox(height: 50), CustomAppBar()]),
+      child: Column(
+        children: [SizedBox(height: 50), CustomAppBar(), CustomListViewItem()],
+      ),
+    );
+  }
+}
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 188,
+
+      decoration: const BoxDecoration(
+        color: Colors.amber,
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('assets/images/test_book_cover_1.jpg'),
+        ),
+      ),
     );
   }
 }
