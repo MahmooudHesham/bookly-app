c37444820d1fd45ca839443bba97d0ae260815e6
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-24 00:03:49 +0200

    add custom listview item to home

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 3 / 5,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage('assets/images/test_book_cover_1.jpg'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,27 +10,12 @@ class HomeViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children: [SizedBox(height: 50), CustomAppBar(), CustomListViewItem()],
-      ),
-    );
-  }
-}
-
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: 188,
-
-      decoration: const BoxDecoration(
-        color: Colors.amber,
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage('assets/images/test_book_cover_1.jpg'),
-        ),
+        children: [
+          SizedBox(height: 50),
+          CustomAppBar(),
+          SizedBox(height: 30),
+          CustomListViewItem(),
+        ],
       ),
     );
   }
