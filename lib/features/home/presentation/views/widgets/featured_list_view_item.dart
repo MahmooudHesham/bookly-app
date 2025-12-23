186c529493bb34c0b1640215444b302f4f4f37f4
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-24 00:30:50 +0200

    add featured list view to home

---
+++ lib/features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3 / 5,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/test_book_cover_1.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
