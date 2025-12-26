544f7281d9df6946470c00ecddb6f426d430d6b5
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-26 16:26:31 +0200

    create custom book details app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,13 +1,19 @@
+import 'package:bookly/features/home/presentation/views/book_detials_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBookDetialsView = '/bookDetailsView';
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
       GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
+      GoRoute(
+        path: kBookDetialsView,
+        builder: (context, state) => const BookDetialsView(),
+      ),
     ],
   );
 }
--- lib/features/home/presentation/views/widgets/best_seller_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_view_item.dart
@@ -1,63 +1,70 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/bood_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 3.2 / 5,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('assets/images/test_book_cover_1.jpg'),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetialsView);
+      },
+      child: SizedBox(
+        height: 130,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 3.2 / 5,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/test_book_cover_1.jpg'),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(width: 30),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    "Dead FLowers",
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kPlayfairDisplay,
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      "Dead FLowers",
+                      style: Styles.textStyle20.copyWith(
+                        fontFamily: kPlayfairDisplay,
+                      ),
                     ),
                   ),
-                ),
 
-                const SizedBox(height: 3),
+                  const SizedBox(height: 3),
 
-                const Text("J.M. Petrick", style: Styles.textStyle14),
+                  const Text("J.M. Petrick", style: Styles.textStyle14),
 
-                const SizedBox(height: 3),
+                  const SizedBox(height: 3),
 
-                const Row(
-                  children: [
-                    Text("19.99 \$", style: Styles.textStyle20),
-                    Spacer(flex: 1),
-                    BookRating(),
-                  ],
-                ),
-              ],
+                  const Row(
+                    children: [
+                      Text("19.99 \$", style: Styles.textStyle20),
+                      Spacer(flex: 1),
+                      BookRating(),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
