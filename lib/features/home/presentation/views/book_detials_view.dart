7018c4dde5d0f5736f2918ddc57dbcb168f064d7
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-26 15:39:27 +0200

    create book details view

---
+++ lib/features/home/presentation/views/book_detials_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/home/presentation/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetialsView extends StatelessWidget {
+  const BookDetialsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: BookDetailsViewBody());
+  }
+}
