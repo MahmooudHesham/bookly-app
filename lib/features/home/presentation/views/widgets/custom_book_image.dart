9868538ff25be376f77d4d5669baf098442ad835
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-29 20:39:21 +0200

    add cahced network image

--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_Image_loading.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -7,12 +9,22 @@ class CustomBookImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 3 / 5,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(fit: BoxFit.fill, image: NetworkImage(imgUrl)),
+      child: ClipRRect(
+        borderRadius: BorderRadiusGeometry.circular(16),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imgUrl,
+          placeholder: (context, url) => const CustomBookImageLoading(),
+          errorWidget: (context, url, error) =>
+              const Icon(Icons.error, color: Colors.red, size: 30),
         ),
       ),
+      //  Container(
+      //   decoration: BoxDecoration(
+      //     borderRadius: BorderRadius.circular(16),
+      //     image: DecorationImage(fit: BoxFit.fill, image: NetworkImage(imgUrl)),
+      //   ),
+      // ),
     );
   }
 }
