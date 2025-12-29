c56eb151e5bcb4dad4c5b0812804bdd3f2805b63
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-29 16:48:14 +0200

    add GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/maneger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/maneger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setupServiceLocater();
   runApp(const BooklyApp());
 }
 
@@ -11,17 +17,27 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'Montserrat'),
-        // textTheme: GoogleFonts.playfairDisplayTextTheme(
-        //   ThemeData.dark().textTheme,
-        // ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: ThemeData.dark().textTheme.apply(fontFamily: 'Montserrat'),
+          // textTheme: GoogleFonts.playfairDisplayTextTheme(
+          //   ThemeData.dark().textTheme,
+          // ),
+        ),
+        //home: const SplashView(),
       ),
-      //home: const SplashView(),
     );
   }
 }
